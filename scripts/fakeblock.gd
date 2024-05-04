7b7bc0bd27f4c56ae9600895afca72cbd6d03091
Atophite <wesley-reinders@outlook.com>
2024-05-04 21:56:40 +0200

    init

---
+++ scripts/fakeblock.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _on_body_entered(body):
+	set_modulate(Color(1, 1, 1, 0.5))
---
+++ scripts/game_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+@onready var score_label = %ScoreLabel
+@onready var hud = %HUD
+
+
+var score = 0
+
+func add_point():
+	score += 1
+	score_label.text = "You collected " + str(score) + " coins."
+	hud.get_node("UIScoreLabel").text = "Coins: " + str(score)
+	print(score)
+
